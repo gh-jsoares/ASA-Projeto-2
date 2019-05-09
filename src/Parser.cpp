b4260682d6be8c93d90ab0f0d326728df5bffdf3
João Soares <jsoares.public@gmail.com>
2019-05-09 18:11:35 +0100

    Init

---
+++ src/Parser.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "Parser.hpp"
+
+void Parser::greet()
+{
+    std::cout << "Hello Penguin man!" << std::endl;
+}
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "../include/Parser.hpp"
+
+int main()
+{
+    Parser p;
+    p.greet();
+}
